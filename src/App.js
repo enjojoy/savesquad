33619c02f03025e9fccf6c9178d2d1882c103aaa
Matthew Foyle <mfoyle93@gmail.com>
2024-05-20 17:39:49 +0200

    feat: new home screen layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { createConfig, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http } from "viem";
 import { mainnet } from "viem/chains";
+
+
+import Main from "./Main";
 
 const config = createConfig({
   chains: [mainnet],
@@ -30,7 +30,7 @@ export default function App() {
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <DynamicWagmiConnector>
-            <DynamicWidget />
+           <Main />
           </DynamicWagmiConnector>
         </QueryClientProvider>
       </WagmiProvider>
