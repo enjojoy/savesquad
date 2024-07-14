8ea3d7cb16cc6205a843e6f879be7201a0a2d69b
Ethan Rouimi <contact@ethan-rouimi.com>
2024-07-14 02:00:01 +0200

    added function for deploying a pool

--- src/views/CreateGroup.js
+++ src/views/CreateGroup.js
@@ -1,20 +1,25 @@
 import { Datepicker } from "flowbite-react";
 import { useState } from "react";
-import lighthouse from '@lighthouse-web3/sdk'
+import lighthouse from "@lighthouse-web3/sdk";
 import EnsResolver from "../components/EnsResolver";
+import { useWriteContract } from "wagmi";
+import {
+  ABI_ROOTSTOCK_POOL_CONTRACT,
+  ROOTSTOCK_POOL_CONTRACT,
+} from "../../pages/constants";
 
 const CreateGroup = ({ groups, setGroups, file, setFile }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
-  const [expirationDate, setExpirationDate] = useState("")
+  const [expirationDate, setExpirationDate] = useState("");
   const [frequency, setFrequency] = useState("weekly");
   const [currency, setCurrency] = useState("USDC");
   const [contribution, setContribution] = useState(0);
   const [handle, setHandle] = useState("");
   const [members, setMembers] = useState([]);
   const [percentageDone, setPercentageDone] = useState(0);
-  const [picHash, setPicHash] = useState("")
+  const [picHash, setPicHash] = useState("");
 
   // useEffect(()=>{
   //   console.log(handle);
@@ -22,11 +27,35 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
 
   // setContribution(0)
 
- 
+  function createPoolTransaction(
+    name,
+    imageCID,
+    amount,
+    frequency,
+    dueDate,
+    members,
+    currency
+  ) {
+    const { writeContract } = useWriteContract();
 
+    writeContract({
+      ABI_ROOTSTOCK_POOL_CONTRACT,
+      address: ROOTSTOCK_POOL_CONTRACT,
+      functonName: "createPool",
+      args: [name, imageCID, amount, frequency, dueDate, members, currency],
+    });
+  }
 
   console.log("GROUPS:", groups);
-  const updateGroups = ({ name, description, amount, currency, contribution, members, picHash }) => {
+  const updateGroups = ({
+    name,
+    description,
+    amount,
+    currency,
+    contribution,
+    members,
+    picHash,
+  }) => {
     const group = {
       name: name,
       desc: description,
@@ -34,24 +63,24 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
       currency: currency,
       contributed: contribution,
       members: members,
-      picHash: picHash
+      picHash: picHash,
     };
-    console.log("New group:", group)
+    console.log("New group:", group);
     setGroups((prev) => {
       const newGroups = [...prev, group];
-      localStorage.setItem('groups', JSON.stringify(newGroups));
+      localStorage.setItem("groups", JSON.stringify(newGroups));
       return newGroups;
-    }
-    )
-
+    });
 
     // TODO: Create a smart contract in celo?
-  }
+  };
   // const handleSubmit = (e) => {
   //   e.preventDefault();
   //   //1. Create new group on Dynamic and add all the members there
   //   //2. Create the group in database with the rest of data(Or retrieve from SC?)
   //   // Assuming groups is an array of group objects
+
+  // call createPoolTransaction(args) to create the pool
 
   //   // Clear the input fields after submission
   //   updateGroups({name, description, amount, currency})
@@ -60,16 +89,24 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
     e.preventDefault();
     const progressCallback = (progressData) => {
       let percentageDone =
-        100 - (progressData?.total / progressData?.uploaded)?.toFixed(2)
-        setPercentageDone(percentageDone);
-      console.log(percentageDone)
-    }
-      console.log("File to upload:", file)
-      const output = await lighthouse.upload([file], "cc17de30.9c81d6383aa5420bb81b027aaa5a5c5d", false, null, progressCallback)
-      const picHash = output.data.Hash;
-      console.log("HASH: ", picHash);
-      setPicHash(picHash);
-        console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash)
+        100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+      setPercentageDone(percentageDone);
+      console.log(percentageDone);
+    };
+    console.log("File to upload:", file);
+    const output = await lighthouse.upload(
+      [file],
+      "cc17de30.9c81d6383aa5420bb81b027aaa5a5c5d",
+      false,
+      null,
+      progressCallback
+    );
+    const picHash = output.data.Hash;
+    console.log("HASH: ", picHash);
+    setPicHash(picHash);
+    console.log(
+      "Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash
+    );
   };
 
   const handleFileChange = async (e) => {
@@ -79,12 +116,22 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
   return (
     <div>
       <h2 className="text-2xl font-press-start mb-4">New squad</h2>
-      < form onSubmit={(e)=>{ 
-        e.preventDefault();
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
 
-        console.log(description)
-        updateGroups({ name, description, amount, currency, contribution, members, picHash })
-      }}>
+          console.log(description);
+          updateGroups({
+            name,
+            description,
+            amount,
+            currency,
+            contribution,
+            members,
+            picHash,
+          });
+        }}
+      >
         <div className="mb-4">
           <label className="block mb-2">Name</label>
           <input
@@ -105,7 +152,6 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
           />
         </div>
         <div className="flex flex-row w-full">
-
           <div className="mb-4 w-3/4 px-2">
             <label className="block mb-2">Squat Goal Amount</label>
             <input
@@ -129,8 +175,7 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
           </div>
         </div>
 
-        <div className="flex flex-row w-full" >
-
+        <div className="flex flex-row w-full">
           <div className="mb-4 w-1/2 px-2">
             <label className="block mb-2">Frequency</label>
             <select
@@ -144,18 +189,18 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
             </select>
           </div>
 
-<div className="mb-4 w-1/2 px-2">
-<label className="block mb-2">Milestone date</label>
-        <Datepicker />
-</div>
+          <div className="mb-4 w-1/2 px-2">
+            <label className="block mb-2">Milestone date</label>
+            <Datepicker />
+          </div>
         </div>
 
         <div className="mb-4">
           <h4>Add squad member</h4>
           <p>Please enter email, eth address or ens handle</p>
           <div className="flex flex-row">
-          <EnsResolver members={members} setMembers={setMembers}/>
-          {/* <input
+            <EnsResolver members={members} setMembers={setMembers} />
+            {/* <input
            type="text"
            value={handle}
            onChange={(e) => setHandle(e.target.value)}
@@ -164,14 +209,25 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
            <button className="bg-azure p-4 ml-2 font-press-start text-white rounded">+</button> */}
           </div>
         </div>
-<div>
+        <div>
+          <input
+            className="font-press-start rounded"
+            type="file"
+            onChange={handleFileChange}
+          />
+          <p>{percentageDone}</p>
+          <button
+            className="font-press-start bg-azure text-white m-2 p-2 rounded"
+            onClick={handleSubmitFile}
+          >
+            Upload
+          </button>
+        </div>
 
-      <input className="font-press-start rounded" type="file" onChange={handleFileChange} />
-           <p>{percentageDone}</p>
-      <button  className="font-press-start bg-azure text-white m-2 p-2 rounded"  onClick={handleSubmitFile}>Upload</button>
-</div>
-
-        <button type="submit"   className={"bg-azure p-4 mt-6 font-press-start text-white rounded"}>
+        <button
+          type="submit"
+          className={"bg-azure p-4 mt-6 font-press-start text-white rounded"}
+        >
           Create squad
         </button>
       </form>
@@ -180,4 +236,3 @@ const CreateGroup = ({ groups, setGroups, file, setFile }) => {
 };
 
 export default CreateGroup;
-
